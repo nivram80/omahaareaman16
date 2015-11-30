a4c65888b3e9e3fb0d99ee7b8fc04c2145c0dc02
Todd Nichols <nivram80@gmail.com>
2015-11-29 21:59:29 -0600

    add background image, menu, and title

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('omahaareaman16', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'toastr']);
+
+})();
